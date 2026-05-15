e084b8365c08b245932a018d2deff31b4a47c775
griffitre <wrgriff01@yahoo.ca>
2026-05-15 17:45:27 -0400

    ADD: viewmodelbase comments

--- Paige/viewmodels/ViewModelBase.cs
+++ Paige/viewmodels/ViewModelBase.cs
@@ -6,13 +6,17 @@ using System.Runtime.CompilerServices;
 
 namespace Paige.viewmodels
 {
+    // Base class for all view models
     public class ViewModelBase : INotifyPropertyChanged
     {
 
+        // The event that is raised when a property's value changes
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // Fires property changed event to notify WPF bindings that a property has changed
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
+            // Automatically pass the name of the property that called this method using name (function parameter)
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
