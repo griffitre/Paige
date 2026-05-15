819b29bb1c364352f86931b9c8a125b949d83093
griffitre <wrgriff01@yahoo.ca>
2026-05-15 16:36:44 -0400

    FEAT: view model base class

---
+++ Paige/viewmodels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace Paige.viewmodels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
