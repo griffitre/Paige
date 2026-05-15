58e920016e91f06b1f0c586baf2ff0a86934f603
griffitre <wrgriff01@yahoo.ca>
2026-05-15 19:10:45 -0400

    FEAT: main menu view model

---
+++ Paige/viewmodels/MainMenuViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using Paige.commands;
+
+namespace Paige.viewmodels
+{
+    // Class for main menu view model
+    public class MainMenuViewModel : ViewModelBase
+    {
+
+        // Declare commands
+        public RelayCommand LogCommand { get; }
+        public RelayCommand CalendarCommand { get; }
+        public RelayCommand JournalCommand { get; }
+        public RelayCommand ExitCommand { get; }
+
+        // Define constructor
+        public MainMenuViewModel()
+        {
+            // Define LogCommand
+            LogCommand = new RelayCommand(() => { });
+
+            // Define CalendarCommand
+            CalendarCommand = new RelayCommand(() => { });
+
+            // Define JournalCommand
+            JournalCommand = new RelayCommand(() => { });
+
+            // Define ExitCommand
+            ExitCommand = new RelayCommand(() => Application.Current.Shutdown());
+        }
+    }
+}
