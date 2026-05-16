bd6fc7fe0d5595256e8f566492a0908eefeb319a
griffitre <wrgriff01@yahoo.ca>
2026-05-15 20:05:06 -0400

    FEAT: short mood entry

---
+++ Paige/models/ShortEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paige.models
+{
+    // Class for a short entry
+    public class ShortEntry
+    {
+        // Date field to store the date it was logged. Automatically is set to the computer's current date
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        // Overall field to store the user's mood from 1-5
+        public int Overall { get; set; }
+
+        // User's optional note
+        public string? Note { get; set; }
+    }
+}
