02b290a190ae81c1fa68a21bfd889a6202cb15ee
griffitre <wrgriff01@yahoo.ca>
2026-05-15 20:07:32 -0400

    ADD: full mood entry file (empty as of now)

---
+++ Paige/models/FullEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paige.models
+{
+    // Full mood entry class that inherits the first three fields from short entry class. This is just a class to store more detailed entries
+    internal class FullEntry : ShortEntry
+    {
+
+    }
+}
