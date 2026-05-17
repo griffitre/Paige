ae737694c8152130e2e1396ff177b614217b6eb0
griffitre <wrgriff01@yahoo.ca>
2026-05-17 04:41:57 -0400

    ADD: updateviewcommand.cs

---
+++ Paige/commands/UpdateViewCommand.cs
@@ -0,0 +1,39 @@
+﻿using Paige.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Paige.commands
+{
+    // Command to handle navigation
+    class UpdateViewCommand : ICommand
+    {
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public UpdateViewCommand(MainWindowViewModel mainWindowViewModel)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+        }
+
+        // Default implementation of ICommand interface
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter.ToString() == "main")
+            {
+                _mainWindowViewModel.ActiveMenu = new MainMenuViewModel(this);
+            }
+            if (parameter.ToString() == "log")
+            {
+                _mainWindowViewModel.ActiveMenu = new ShortLogViewModel();
+            }
+        }
+    }
+}
