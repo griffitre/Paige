bf8f00c4af99093130cc7d5167d2ea2a41f0c490
griffitre <wrgriff01@yahoo.ca>
2026-05-18 21:37:51 -0400

    FEAT: loadall method to load all logs from the user's json file

--- Paige/services/DataService.cs
+++ Paige/services/DataService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using System.IO;
+using Paige.models;
 
 namespace Paige.services
 {
@@ -28,6 +29,21 @@ namespace Paige.services
             string _filepath = Path.Combine(roamPath, "entries.json");
         }
 
-                
+        // Method to load logs from a json file
+        public List<ShortEntry> LoadAll()
+        {
+            // Check if the file exists. If it doesnt, just return an empty short entry list
+            if (!File.Exists(_filePath))
+            {
+                return new List<ShortEntry>();
+            }
+
+            // Otherwise, read all the serialized data to a string
+            string serializedString = File.ReadAllText(_filePath);
+
+            // Then, return the deserialized version of it
+            return JsonSerializer.Deserialize<List<ShortEntry>>(serializedString);
+
+        }
     }
 }
