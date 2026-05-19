15334a7cd26fbe5e66902f5b1db74d06c856ffa9
griffitre <wrgriff01@yahoo.ca>
2026-05-18 21:19:43 -0400

    FEAT: dataservice class + constructor that finds the app data folder

--- Paige/services/DataService.cs
+++ Paige/services/DataService.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.IO;
 
 namespace Paige.services
 {
-    internal class DataService
+    // Class that handles saving and loading data
+    public class DataService
     {
+
+        // Create a private readonly string to store the filepath
+        private readonly string _filePath;
+
+        public DataService()
+        {
+            // Get the path to the app data folder
+            string roamPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            // Create the full directory path
+            string direcPath = Path.Combine(roamPath, "Paige");
+
+            // Create/ensure the folder is already created
+            Directory.CreateDirectory(direcPath);
+
+            // Create the full file path
+            string _filepath = Path.Combine(roamPath, "entries.json");
+        }
+
+                
     }
 }
