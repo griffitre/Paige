a06f766a5d00481b2eb81dca3014e9dec2f20397
griffitre <wrgriff01@yahoo.ca>
2026-05-19 20:24:30 -0400

    ADD: calendarviewmodel

---
+++ Paige/viewmodels/CalendarViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paige.viewmodels
+{
+    // Defintion of CalendarViewModel
+    public class CalendarViewModel
+    {
+    }
+}
