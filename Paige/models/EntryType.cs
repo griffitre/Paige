bfb6d562a42a67400d5174ff561d7a9437ac1cbd
griffitre <wrgriff01@yahoo.ca>
2026-05-19 20:36:59 -0400

    ADD: entrytype.cs model

---
+++ Paige/models/EntryType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paige.models
+{
+    // Definition of EntryType
+    public enum EntryType
+    {
+        Short,
+        Full
+    }
+}
