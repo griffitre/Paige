886c4689fd562d6126674a965498ddbc1518b518
griffitre <wrgriff01@yahoo.ca>
2026-05-20 17:23:47 -0400

    FEAT: moodimagehelper implemented

--- Paige/helpers/MoodImageHelper.cs
+++ Paige/helpers/MoodImageHelper.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Paige.helpers
 {
-    internal class MoodImageHelper
+    // Definition for MoodImageHelper
+    public static class MoodImageHelper
     {
+        // Create the dictionary to translate mood scores to the respective image
+        private static readonly Dictionary<int, string> MoodImages = new()
+        {
+            { 1, "/assets/Mood1Placeholder.png" },
+            { 2, "/assets/Mood2Placeholder.png" },
+            { 3, "/assets/Mood3Placeholder.png" },
+            { 4, "/assets/Mood4Placeholder.png" },
+            { 5, "/assets/Mood5Placeholder.png" }
+        };
+
+        // Method to get the respective image given the score. Default to neutral (mood 3) if not found (prevents crashes)
+        public static string GetImage(int score)
+        {
+            return MoodImages.TryGetValue(score, out string image) ? image : "/assets/Mood3Placeholder";
+        }
     }
 }
