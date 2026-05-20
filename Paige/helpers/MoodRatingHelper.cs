6d09db01d762fd42bb5b0c4f93a08d7c6a3f2879
griffitre <wrgriff01@yahoo.ca>
2026-05-20 17:33:53 -0400

    FEAT: moodratinghelper implemented

--- Paige/helpers/MoodRatingHelper.cs
+++ Paige/helpers/MoodRatingHelper.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Paige.helpers
 {
-    internal class MoodRatingHelper
+    // Definition for MoodRatingHelper
+    public static class MoodRatingHelper
     {
+        // Create the dictionary to translate mood scores to the respective rating
+        private static readonly Dictionary<int, string> MoodRatings = new()
+        {
+            { 1, "Awful" },
+            { 2, "Sad" },
+            { 3, "Okay" },
+            { 4, "Good" },
+            { 5, "Great" }
+        };
+
+        // Method to get the respective rating given the score. Default to neutral (Okay) if not found (prevents crashes)
+        public static string GetRating(int score)
+        {
+            return MoodRatings.TryGetValue(score, out string rating) ? rating : "Okay";
+        }
     }
 }
