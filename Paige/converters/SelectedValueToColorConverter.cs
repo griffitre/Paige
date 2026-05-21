7fb83a664e4d494c49473b17a61ef40a9cf38303
griffitre <wrgriff01@yahoo.ca>
2026-05-20 20:58:05 -0400

    REFACTOR: change in selected button colour

--- Paige/converters/SelectedValueToColorConverter.cs
+++ Paige/converters/SelectedValueToColorConverter.cs
@@ -14,7 +14,7 @@ namespace Paige.converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             // If a button's value matches the selected value, return the highlight color
-            return value?.ToString() == parameter.ToString() ? new SolidColorBrush(Colors.Gray) : new SolidColorBrush(Colors.White);
+            return value?.ToString() == parameter.ToString() ? new SolidColorBrush(Colors.LightGray) : new SolidColorBrush(Colors.White);
         }
 
         // Other method required by interface (unused)
