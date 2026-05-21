eb480ac10bfa24bf88b9b2eff5994e22f6c6350b
griffitre <wrgriff01@yahoo.ca>
2026-05-20 21:55:25 -0400

    FEAT: fullentry implemented

--- Paige/models/FullEntry.cs
+++ Paige/models/FullEntry.cs
@@ -4,9 +4,32 @@ using System.Text;
 
 namespace Paige.models
 {
-    // Full mood entry class that inherits the first three fields from short entry class. This is just a class to store more detailed entries
-    internal class FullEntry : ShortEntry
+    // Full mood entry class that inherits the first three fields from short entry class
+    public class FullEntry : ShortEntry
     {
+        // Overwrite the inherited EntryType (just set it again, this time to full instead of short)
+        public EntryType EntryType { get; set; } = EntryType.Full;
 
+        // Properties
+        // Hobbies
+        public List<int> Hobbies { get; set; } = new List<int>();
+
+        // Meals
+        public List<int> Meals { get; set; } = new List<int>();
+
+        // Chores
+        public List<int> Chores { get; set; } = new List<int>();
+
+        // SelfCare
+        public List<int> SelfCare { get; set; } = new List<int>();
+
+        // Social
+        public List<int> Social { get; set; } = new List<int>();
+
+        // RecreationalUse
+        public List<int> RecreationalUse { get; set; } = new List<int>();
+
+        // Weather
+        public List<int> Weather { get; set; } = new List<int>();
     }
 }
