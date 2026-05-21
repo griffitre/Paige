812ea5c845c98c6f1edc5fbca2f71f2524d7b6c1
griffitre <wrgriff01@yahoo.ca>
2026-05-20 20:52:30 -0400

    FEAT: selectedvaluetocolourconverter implemented

--- Paige/converters/SelectedValueToColorConverter.cs
+++ Paige/converters/SelectedValueToColorConverter.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Paige.converters
 {
-    internal class SelectedValueToColorConverter
+    // Definition of SelectedValueToColorConverter
+    public class SelectedValueToColorConverter : IValueConverter
     {
+        // Convert method
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If a button's value matches the selected value, return the highlight color
+            return value?.ToString() == parameter.ToString() ? new SolidColorBrush(Colors.Gray) : new SolidColorBrush(Colors.White);
+        }
+
+        // Other method required by interface (unused)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
