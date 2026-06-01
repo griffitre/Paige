a446a8a1b6b9a24a171978b87478f63501d935aa
griffitre <wrgriff01@yahoo.ca>
2026-05-31 20:42:58 -0400

    FEAT: journaldetailviewmodel implemented

--- Paige/viewmodels/JournalDetailViewModel.cs
+++ Paige/viewmodels/JournalDetailViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Paige.commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Navigation;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.IO;
+using Paige.models;
+using System.Drawing;
 
 namespace Paige.viewmodels
 {
-    internal class JournalDetailViewModel
+    // Definition of the viewmodel for journaldetailview
+    public class JournalDetailViewModel : ViewModelBase
     {
+        // Create a JournalEntry to be passed from the journal calendar view
+        
+
+        // Declare BackCommand
+        public RelayCommand BackCommand { get; }
+
+
+        // Expose all possible fields as read only
+        public string JournalDay { get; }
+        public string? JournalText { get; }
+
+
+        // Constructor
+        public JournalDetailViewModel(UserJournalEntry entry, ICommand updateViewCommand)
+        {
+            // Define BackCommand
+            BackCommand = new RelayCommand(() => updateViewCommand.Execute("main"));
+
+            // Set JournalDay
+            JournalDay = entry.Date.ToString("MMMM dd, yyyy");
+
+            // Set JournalText
+            JournalText = entry.JournalBody;
+        }
     }
 }
