05353f509f9d266b70f66632b328c3238ef62015
griffitre <wrgriff01@yahoo.ca>
2026-05-31 22:24:45 -0400

    FEAT: journalselectviewmodel implemented

--- Paige/viewmodels/JournalSelectViewModel.cs
+++ Paige/viewmodels/JournalSelectViewModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Text;
+using Paige.commands;
+using System.Windows.Input;
 
 namespace Paige.viewmodels
 {
-    internal class JournalSelectViewModel
+    // Definition of the viewmodel for journalselectview
+    public class JournalSelectViewModel : ViewModelBase
     {
+        // Declare commands for the buttons
+        public RelayCommand BackCommand { get; }
+        public RelayCommand CalendarCommand { get; }
+        public RelayCommand JournalCommand { get; }
+
+        public JournalSelectViewModel(ICommand updateViewCommand)
+        {
+            // If back button is clicked, go to main menu
+            BackCommand = new RelayCommand(() => updateViewCommand.Execute("main"));
+
+            // If calendar button is clicked, go to journal calendar view (not yet implemented)
+            CalendarCommand = new RelayCommand(() => { });
+
+            // If the edit button is clicked, go to the journal view
+            JournalCommand = new RelayCommand(() => updateViewCommand.Execute("journal"));
+        }
     }
 }
