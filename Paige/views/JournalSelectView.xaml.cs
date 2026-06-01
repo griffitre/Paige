99a32f3544e171e427fe31f288fc3daf74034a39
griffitre <wrgriff01@yahoo.ca>
2026-05-31 22:17:58 -0400

    FEAT: journalselectview implemented

--- Paige/views/JournalSelectView.xaml.cs
+++ Paige/views/JournalSelectView.xaml.cs
@@ -13,13 +13,11 @@ using System.Windows.Shapes;
 
 namespace Paige.views
 {
-    /// <summary>
-    /// Interaction logic for JournalSelectView.xaml
-    /// </summary>
     public partial class JournalSelectView : UserControl
     {
         public JournalSelectView()
         {
+            // Initialize
             InitializeComponent();
         }
     }
