3391d770bf9499d78327cc5e60ad1d36e17853c2
griffitre <wrgriff01@yahoo.ca>
2026-06-09 01:11:22 -0400

    REFACTOR: unused using statements and remaining statements organized in converters folder

--- Paige/converters/NullToVisibilityConverter.cs
+++ Paige/converters/NullToVisibilityConverter.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Data;
+using System.Globalization;
 
 namespace Paige.converters
 {
