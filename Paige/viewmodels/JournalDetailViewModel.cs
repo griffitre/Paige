453761c5b4c16159919a3e2971448c87ee75069e
griffitre <wrgriff01@yahoo.ca>
2026-06-13 20:26:39 -0400

    FEAT: delete functionality implemented in journaldetailview/viewmodel

--- Paige/viewmodels/JournalDetailViewModel.cs
+++ Paige/viewmodels/JournalDetailViewModel.cs
@@ -1,18 +1,26 @@
-﻿using System.Windows.Input;
-using Paige.commands;
+﻿using Paige.commands;
 using Paige.models;
+using Paige.services;
+using System.Windows.Input;
 
 namespace Paige.viewmodels
 {
     // Definition of the viewmodel for journaldetailview
     public class JournalDetailViewModel : ViewModelBase
     {
+        // JournalDataService field to load delete the entry thats being viewed
+        private readonly JournalDataService _journalDataService = new JournalDataService();
+
+
         // Commands
         // Declare command to go back
         public RelayCommand BackCommand { get; }
 
         // Declare command to go back to the main menu
         public RelayCommand ExitCommand { get; }
+
+        // Declare command to delete the entry thats being viewed
+        public RelayCommand DeleteCommand { get; }
 
 
         // Properties to expose + make them bindable
@@ -62,6 +70,20 @@ namespace Paige.viewmodels
 
             // Define ExitCommand
             ExitCommand = new RelayCommand(() => updateViewCommand.Execute("main"));
+
+            // Define DeleteCommand to run the delete entry, then run return action, defaulting to the main menu if the return action is not given
+            DeleteCommand = new RelayCommand(() =>
+            {
+                DeleteEntry(entry, updateViewCommand);
+                if (returnAction != null)
+                {
+                    returnAction();
+                }
+                else
+                {
+                    updateViewCommand.Execute("main");
+                }
+            });
         }
 
 
@@ -74,6 +96,13 @@ namespace Paige.viewmodels
 
             // Set JournalText
             JournalText = entry.JournalBody;
+        }
+
+        // Method to delete the entry thats being viewed
+        private void DeleteEntry(UserJournalEntry entry, ICommand updateViewCommand)
+        {
+            // Delete the entry
+            _journalDataService.Delete(entry);
         }
     }
 }
