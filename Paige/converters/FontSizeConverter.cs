71ab2b593299706e58acd0a834c30a60ab09e074
griffitre <wrgriff01@yahoo.ca>
2026-06-15 19:12:48 -0400

    FEAT: different text type converters implemented in fontsizeconverter

--- Paige/converters/FontSizeConverter.cs
+++ Paige/converters/FontSizeConverter.cs
@@ -6,8 +6,8 @@ namespace Paige.converters
     // Converter to dynamically change font size based on window size
     public class FontSizeConverter : IValueConverter
     {
-        // Returns the calculated font size for text on screen
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        // Returns the calculated font size for text on screen using the window width and a given ratio. Acts as the base class for the next converters
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             // Get the width of the window
             double windowWidth = (double)value;
@@ -15,14 +15,66 @@ namespace Paige.converters
             // Get the ratio passed
             double ratio = double.Parse(parameter.ToString());
 
-            // Return the value, or the given max value
-            return Math.Max(10, windowWidth * ratio);
+            // Return the width * ratio
+            return windowWidth * ratio;
         }
 
         // Other item of the interface (unneeded)
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    // Converter for header text 1
+    public class HeaderText1Converter : FontSizeConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the returned value from FontSizeConverter
+            double returnedSize = (double)base.Convert(value, targetType, parameter, culture);
+
+            // Return either 25 or the returned size
+            return Math.Max(25, returnedSize);
+        }
+    }
+
+    // Converter for header text 2
+    public class HeaderText2Converter : FontSizeConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the returned value from FontSizeConverter
+            double returnedSize = (double)base.Convert(value, targetType, parameter, culture);
+
+            // Return either 20 or the returned size
+            return Math.Max(20, returnedSize);
+        }
+    }
+
+    // Converter for normal text
+    public class NormalTextConverter : FontSizeConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the returned value from FontSizeConverter
+            double returnedSize = (double)base.Convert(value, targetType, parameter, culture);
+
+            // Return either 15 or the returned size
+            return Math.Max(15, returnedSize);
+        }
+    }
+
+    // Converter for subtext
+    public class SubtextConverter : FontSizeConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the returned value from FontSizeConverter
+            double returnedSize = (double)base.Convert(value, targetType, parameter, culture);
+
+            // Return either 10 or the returned size
+            return Math.Max(10, returnedSize);
         }
     }
 }
