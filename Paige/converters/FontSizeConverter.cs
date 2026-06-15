e2df6bae3bdd781a788620a94cf412f5739e4b2d
griffitre <wrgriff01@yahoo.ca>
2026-06-15 17:46:09 -0400

    FEAT: fontsizeconverter implemented using window width

--- Paige/converters/FontSizeConverter.cs
+++ Paige/converters/FontSizeConverter.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Windows.Data;
 
 namespace Paige.converters
 {
-    internal class FontSizeConverter
+    // Converter to dynamically change font size based on window size
+    public class FontSizeConverter : IValueConverter
     {
+        // Returns the calculated font size for text on screen
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the width of the window
+            double windowWidth = (double)value;
+
+            // Get the ratio passed
+            double ratio = double.Parse(parameter.ToString());
+
+            // Return the value, or the given max value
+            return Math.Max(10, windowWidth * ratio);
+        }
+
+        // Other item of the interface (unneeded)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
