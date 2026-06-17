26e88fc9870415ec2956c531e73cdd7af84df8be
griffitre <wrgriff01@yahoo.ca>
2026-06-17 14:57:40 -0400

    REFACTOR: dimensionsizeconverter is now an multivalueconverter and handles a passed minimum value

--- Paige/converters/DimensionSizeConverter.cs
+++ Paige/converters/DimensionSizeConverter.cs
@@ -4,24 +4,27 @@ using System.Windows.Data;
 namespace Paige.converters
 {
     // Converter to dynamically return dimensions based on window size
-    public class DimensionSizeConverter : IValueConverter
+    public class DimensionSizeConverter : IMultiValueConverter
     {
-        // Returns either a default size or a calculated size, depending on whether or not the calculated size is larger than the minimum
+        // Returns either a minimum size or a calculated size, depending on whether or not the calculated size is larger than the minimum
         // Primarily used to determine row/column sizes in a grid
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             // Get the passed value
-            double givenValue = (double)value;
+            double givenValue = (double)values[0];
 
             // Get the ratio
             double ratio = double.Parse(parameter.ToString());
 
+            // Get the minimum size
+            double minimumValue = (double)values[1];
+
             // Return either the minimum value or the calculated value, whichever is larger
-            return Math.Max(300, givenValue * ratio);
+            return Math.Max(minimumValue, givenValue * ratio);
         }
 
         // Other item of the interface (unneeded)
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
