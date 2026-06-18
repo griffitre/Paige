2fa2319270c667482e151b5a3012f169403c586c
griffitre <wrgriff01@yahoo.ca>
2026-06-18 01:00:22 -0400

    FEAT: dimensionsizeconverter now handles a max value

--- Paige/converters/DimensionSizeConverter.cs
+++ Paige/converters/DimensionSizeConverter.cs
@@ -6,7 +6,7 @@ namespace Paige.converters
     // Converter to dynamically return dimensions based on window size
     public class DimensionSizeConverter : IMultiValueConverter
     {
-        // Returns either a minimum size or a calculated size, depending on whether or not the calculated size is larger than the minimum
+        // Returns either a minimum size or a calculated size OR a max size, depending on whether or not the calculated size is larger than the minimum
         // Primarily used to determine row/column sizes in a grid
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
@@ -19,8 +19,14 @@ namespace Paige.converters
             // Get the minimum size
             double minimumValue = (double)values[1];
 
+            // Get the maximum size
+            double maximumValue = (double)values[2];
+
+            // Define calculatedValue and assign it the value of either the givenValue * ratio or the max value, depending on which is smaller
+            double calculatedValue = Math.Min(maximumValue, givenValue * ratio);
+
             // Return either the minimum value or the calculated value, whichever is larger
-            return Math.Max(minimumValue, givenValue * ratio);
+            return Math.Max(minimumValue, calculatedValue);
         }
 
         // Other item of the interface (unneeded)
