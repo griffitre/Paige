d2f77ab9e139b1c937251bd8816a60c0bacef712
griffitre <wrgriff01@yahoo.ca>
2026-06-21 14:11:36 -0400

    FEAT: savedmenuviewmodel implemented

--- Paige/viewmodels/SavedMenuViewModel.cs
+++ Paige/viewmodels/SavedMenuViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Paige.viewmodels
 {
-    class SavedMenuViewModel
+    // Definition of the viewmodel for savedmenuview
+    public class SavedMenuViewModel : ViewModelBase
     {
+        // Constructor
+        public SavedMenuViewModel(ICommand updateViewCommand)
+        {
+            // Wait 3 seconds, then send the user back to the main menu
+            Thread.Sleep(3000);
+            updateViewCommand.Execute("main");
+        }
     }
 }
